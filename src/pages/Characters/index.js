e237c157b07bf73427f8cf74ef39110f8b079783
Guilherme Moraes <gmoraes1@outlook.com>
2020-09-11 11:06:46 -0300

    Creating Character Unavailable if doesn`t exist

--- src/pages/Characters/index.js
+++ src/pages/Characters/index.js
@@ -12,27 +12,34 @@ function Characters() {
 
   const name = useLocation().state;
   const [characters, setCharacters] = useState([]);
+  const [unavailable, setUnavailable] = useState(false)
+  const [loading, setLoading] = useState(true)
 
   let timestamp = Date.now().toString();
 
   const publicKey = '145ddbf92c598028adbd37280fca398b';
   const privateKey = 'fe34d571db8241d4961eef238845439e625f8c21';
-  let hash = CryptoJs.MD5(timestamp + privateKey + publicKey)
+  let hash = CryptoJs.MD5(timestamp + privateKey + publicKey);
+  
 
   useEffect(() => {
     axios.get(`https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${name}&ts=${timestamp}&apikey=145ddbf92c598028adbd37280fca398b&hash=${hash}`)
       .then(resp => {
         const array = resp.data.data.results;
         setCharacters(array)
-
+        if(array.length <1) {
+          setUnavailable(true)
+        }
+        setLoading(false)
 
       }).catch(erro => {
         console.log(erro)
       })
   }, [])
 
-  if (characters.length < 1) {
-    return (
+  if (loading) {
+
+      return (
       <div id="characters-container">
         <Header title='Marvel Characters' />
         <div className="unavailable">
@@ -45,29 +52,49 @@ function Characters() {
           </LoadingOverlay>
         </div>
       </div>
-    )
+    ) 
+    
+
   } else {
-    return (
-      <div id="characters-container">
-        <Header title='Marvel Characters' />
-        <div className="cards">
-          {
+    if(unavailable) {
 
-            characters.map((element) => (
-
-              <Card title={element.name} key={element.id} description={element.description}
-                photo={`${element.thumbnail.path}.${element.thumbnail.extension}`}
-                route='characters-description' value={element}
-              />
-
-            ))
-          }
-
+      return (
+        <div id="characters-container">
+          <Header title='Marvel Characters' />
+          <div className="unavailable">
+            <p>Character Unavailable</p>
+          </div>
         </div>
-
-      </div>
-    )
+      ) 
+      
+    } else {
+      return (
+        <div id="characters-container">
+          <Header title='Marvel Characters' />
+          <div className="cards">
+            {
+  
+              characters.map((element) => (
+  
+                <Card title={element.name} key={element.id} description={element.description}
+                  photo={`${element.thumbnail.path}.${element.thumbnail.extension}`}
+                  route='characters-description' value={element}
+                />
+  
+              ))
+            }
+  
+          </div>
+  
+        </div>
+      )
+    }
+    
   }
+
+  
+
+ 
 
 
 
